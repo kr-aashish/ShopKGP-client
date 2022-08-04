52007c6a306938ccf937da68ee272f021de59ab0
Sai Chandan Kadarla <chandankadarla2722002@gmail.com>
2022-08-04 11:27:52 +0530

    Added Account Page, Theme, Router, mui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-import Header from './components/Header';
+import { ThemeProvider } from "@emotion/react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Account from "./pages/account/Account";
+import { darkTheme } from "./themes/dark";
+import { lightTheme } from "./themes/light";
 
 function App() {
   return (
     <div className="app">
-      <Header/>
+      <ThemeProvider theme={lightTheme}>
+        <Routes>
+          <Route exact path="/" element={<Header />} />
+          <Route exact path="/account/:id" element={<Account />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
