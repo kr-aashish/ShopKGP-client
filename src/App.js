e9f471d1a2a325c3c98b3696c95585fa1e24b74b
kr-aashish <jmphedaashish@gmail.com>
2022-08-04 08:37:40 +0530

    Creating header for the home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets Build the KGP's internal OLX 🚀</h1>
+      <Header/>
     </div>
   );
 }
